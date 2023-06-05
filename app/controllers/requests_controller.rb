dd6775fc233671435d86cdafa8d7e727561be965
hiu lam tse <hiulam1@hotmail.com>
2023-06-05 16:30:20 +0200

    added basic CRUD functions on requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,73 @@
 class RequestsController < ApplicationController
+  before_action :set_pet, only: %i[new create]
+  before_action :set_request, only: %i[show edit update destroy confirm]
+  # skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  def index
+    @requests = Request.all
+  end
+
+  def show; end
+
+  def confirm
+    @request.confirmed!
+    if @request.confirmed!
+      flash[:notice] = 'Request confirmed successfully'
+    else
+      flash[:alert] = 'There was an error confirming this request'
+    end
+  end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+    @request.pet = @pet
+
+    respond_to do |format|
+      if @request.save
+        format.html { redirect_to request_url(@request), notice: "Request was successfully created." }
+        format.json { render :show, status: :created, location: @pet }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @request.update(request_params)
+        format.html { redirect_to request_url(@request), notice: "request was successfully updated." }
+        format.json { render :show, status: :ok, location: @request }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @request.destroy
+    redirect_to requests_path, notice: 'request was successfully deleted.'
+  end
+
+  private
+
+  def set_request
+    @request = request.find(params[:id])
+  end
+
+  def request_params
+    params.require(:request).permit(:title, :visit_date, :message)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
 end
