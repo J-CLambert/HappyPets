7a4ec4526e531e7122de99061fb6a83b8643f43c
J-CLambert <122529617+J-CLambert@users.noreply.github.com>
2023-06-06 15:21:34 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  def age(birthday)
-    days = (Date.today - birthday).to_i
-    years = (days / 365).floor
-    months = (days % 365) / 30
-    "#{years} years and #{months} months"
-  end
 end
