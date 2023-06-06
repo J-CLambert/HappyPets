81e728945b22825f9c96ad3b96be0b0be0dd7f16
hiu lam tse <hiulam1@hotmail.com>
2023-06-06 15:17:50 +0200

    pet index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def age(birthday)
+    days = (Date.today - birthday).to_i
+    years = (days / 365).floor
+    months = (days % 365) / 30
+    "#{years} years and #{months} months"
+  end
 end
