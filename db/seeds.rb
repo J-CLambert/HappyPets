289db35d9c215b214fc79d737442021ffef12a91
Lambert JC <jam@jumpyone.ch>
2023-06-13 16:54:00 +0200

    2 cat

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,28 +45,54 @@ puts 'Creating 1 fake pets per user...'
 
   photos_cat_1 = []
   photos_cat_1 << {
-    io: URI.open("https://wannacat.org/wp-content/uploads/2020/12/DSC03387-2.jpg"),
-    filename: 'photo_cat_1_1.png',
+    io: File.open("../../app/assets/images/all_cats/cat1/photo-1570419868699-b5e996ef2796.avif"),
+    filename: 'photo_cat1_1.png',
     content_type: 'image/png'
   }
   photos_cat_1 << {
-    io: URI.open("https://images.prismic.io/trustedhousesitters/ee6e6fb5-a9a3-402a-9af5-314fabe7c69a_siamese+cat.png?auto=compress,format&rect=0,0,1920,800&w=960&h=400"),
-    filename: 'photo_cat_1_2.png',
+    io: File.open("../../app/assets/images/all_cats/cat1/photo-1582530182198-98b5312880f6.avif"),
+    filename: 'photo_cat1_2.png',
     content_type: 'image/png'
   }
-  file_cat = URI.open("https://www.rd.com/wp-content/uploads/2021/04/GettyImages-145679137-scaled-e1619025176434.jpg?w=2515")
     pet = Pet.new(
-      name: Faker::Name.name,
-      price: Faker::Commerce.price(range: 10..50),
-      breed: Faker::Creature::Horse.breed,
-      description: Faker::Quote.yoda,
-      title: Faker::Quote.famous_last_words,
-      birthday: Faker::Date.between(from: '1940-09-23', to: Date.today),
-      vaccinated_against: Faker::JapaneseMedia::StudioGhibli.character,
+      name: "Nala",
+      price: Faker::Commerce.price(range: 10..500),
+      breed: "European Shorthair",
+      description: "This cat has a personality as vibrant as a sunbeam. Social and playful, it loves being the star of the show and will seize every opportunity to capture attention, whether by playing with its favourite toys or hopping on your lap when you're trying to work. This cat's antics never fail to bring a smile.",
+      title: "Nala",
+      birthday: Faker::Date.between(from: '2020-09-23', to: Date.today),
+      vaccinated_against: "Typhus, Coryza, FeLV",
       user_id: User.pluck(:id).sample,
-      species: species.shuffle.first,
+      species: "cat",
     )
   pet.photos.attach(photos_cat_1)
+  pet.save
+
+
+
+  photos_cat2 = []
+  photos_cat2 << {
+    io: File.open("../../app/assets/images/all_cats/cat2/photo-1549878569-a8105b1188b8.avif"),
+    filename: 'photo_cat2_1.png',
+    content_type: 'image/png'
+  }
+  photos_cat2 << {
+    io: File.open("../../app/assets/images/all_cats/cat2/photo-1572590603603-f828f6682212.avif"),
+    filename: 'photo_cat2_2.png',
+    content_type: 'image/png'
+  }
+    pet = Pet.new(
+      name: "Félix",
+      price: Faker::Commerce.price(range: 10..500),
+      breed: "siamese",
+      description: "Gentle and loving, this feline is the embodiment of affection. It will spend countless hours purring in your lap, always ready for a cuddle. But when it's not dozing off, it enjoys a good game of hide-and-seek or chasing after a piece of string. This cat's combination of curiosity and affection makes it the perfect companion.",
+      title: "Félix",
+      birthday: Faker::Date.between(from: '2020-09-23', to: Date.today),
+      vaccinated_against: "Coryza,Typhus",
+      user_id: User.pluck(:id).sample,
+      species: "cat",
+    )
+  pet.photos.attach(photos_cat2)
   pet.save
 
   puts 'Finish creating fake pets per user.'
